e49e765d58496ffeb3e0a9dc7fcf7787a07d2f63
nruhovets <dakeeper32@gmail.com>
2022-04-24 19:46:07 -0700

    Complete Excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let finalString = "";
+
+    for(i=string.length -1; i>=0; i--){
+        currentChar = string.charAt(i);
+        finalString += currentChar;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
