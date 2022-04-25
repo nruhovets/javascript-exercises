cd677681fd04e9e1053a7bc3a59ce75ba3572a71
nruhovets <dakeeper32@gmail.com>
2022-04-24 19:35:21 -0700

    Complete Excercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTimesToRepeat){
+    
+    let finalString = "";
 
+    if (numberOfTimesToRepeat < 0){
+        return "ERROR";
+    }
+    for(i=0;i<numberOfTimesToRepeat;i++){
+        finalString += string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
