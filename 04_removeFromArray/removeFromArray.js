6ecc6d43fb8e15254e1351d4893c660eb95fb65f
nruhovets <dakeeper32@gmail.com>
2022-04-28 17:49:32 -0700

    Complete Excercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    
+for(i=0;i<array.length; i++){
+    for (j=0; j<args.length; j++){
+        if(array[i] === args[j]){
+            array.splice(i,1)
+            i--;
+        }
+    }
+}
+return array;
 };
 
 // Do not edit below this line
