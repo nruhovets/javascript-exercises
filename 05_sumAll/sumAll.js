a35c26f0cc66e5f4055a17395d5d51e159903f36
nruhovets <dakeeper32@gmail.com>
2022-04-30 11:17:04 -0700

    Complete Excercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,29 @@
-const sumAll = function() {
+const sumAll = function(lowRangeInt, highRangeInt) {
+    finalSum = 0;
 
+    if (highRangeInt < 0 || lowRangeInt < 0){
+        return "ERROR";
+    }
+    else if (typeof highRangeInt != "number" || typeof lowRangeInt != "number"){
+        return "ERROR";
+    }
+    else if (lowRangeInt <= highRangeInt){
+        for (i=lowRangeInt; i<=highRangeInt; i++){
+            finalSum += i;
+        }
+        return finalSum;
+    }
+    else if (highRangeInt <= lowRangeInt){
+        for (i=highRangeInt; i<=lowRangeInt; i++){
+            finalSum+= i;
+        }
+        return finalSum;
+    }
+    else 
+    return "ERROR";
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
