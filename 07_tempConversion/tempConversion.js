f0e015a046b3c01e36a36cabcb8b116a5f7f28db
nruhovets <dakeeper32@gmail.com>
2022-04-30 14:32:16 -0700

    Complete Excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temperatureInFahrenheit) {
+  return Math.round(((temperatureInFahrenheit - 32) / 1.8) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temperatureInCelcius) {
+  return Math.round((temperatureInCelcius * 1.8 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
