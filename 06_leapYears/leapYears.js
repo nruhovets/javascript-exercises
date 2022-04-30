a52d29a8f950633d8c79f6e790feb41a9964449f
nruhovets <dakeeper32@gmail.com>
2022-04-30 14:22:04 -0700

    Rework Solution to be Concise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,20 +1,5 @@
 const leapYears = function(year) {
-    if (typeof year != "number"){
-        return "ERROR";
-    }
-    else if (year % 400 === 0){
-        return true;
-    }
-    else if (year % 100 === 0){
-        return false;
-    }
-    else if (year % 4 === 0){
-        return true;
-    }
-    else{
-        return false;
-    }
-
+return (year % 4 === 0 && (year % 100 !==0)|| (year % 400 === 0));
 };
 
 // Do not edit below this line
